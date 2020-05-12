0c5ce02cce36c8320aed46018c1f0c88cee8921d
SteveYurongSu <steveyurongsu@outlook.com>
2020-05-12 18:50:04 +0800

    modify grammer keyValuePair

--- server/src/main/java/org/apache/iotdb/db/qp/strategy/LogicalGenerator.java
+++ server/src/main/java/org/apache/iotdb/db/qp/strategy/LogicalGenerator.java
@@ -572,7 +572,6 @@ public class LogicalGenerator extends SqlBaseBaseListener {
     queryOp.setFill(true);
     queryOp.setLeftCRightO(ctx.timeInterval().LS_BRACKET() != null);
 
-
     // parse timeUnit
     queryOp.setUnit(parseDuration(ctx.DURATION().getText()));
     queryOp.setSlidingStep(queryOp.getUnit());
@@ -857,7 +856,7 @@ public class LogicalGenerator extends SqlBaseBaseListener {
     if (ctx.property(0) != null) {
       for (PropertyContext property : properties) {
         props.put(property.ID().getText().toLowerCase(),
-                property.propertyValue().getText().toLowerCase());
+            property.propertyValue().getText().toLowerCase());
       }
     }
     createTimeSeriesOperator.setCompressor(compressor);
@@ -873,7 +872,7 @@ public class LogicalGenerator extends SqlBaseBaseListener {
     Map<String, String> attributes = new HashMap<>(attributesList.size());
     if (ctx.property(0) != null) {
       for (PropertyContext property : attributesList) {
-        if(property.propertyValue().STRING_LITERAL() != null) {
+        if (property.propertyValue().STRING_LITERAL() != null) {
           value = removeStringQuote(property.propertyValue().getText());
         } else {
           value = property.propertyValue().getText();
@@ -893,7 +892,7 @@ public class LogicalGenerator extends SqlBaseBaseListener {
     Map<String, String> tags = new HashMap<>(tagsList.size());
     if (ctx.property(0) != null) {
       for (PropertyContext property : tagsList) {
-        if(property.propertyValue().STRING_LITERAL() != null) {
+        if (property.propertyValue().STRING_LITERAL() != null) {
           value = removeStringQuote(property.propertyValue().getText());
         } else {
           value = property.propertyValue().getText();
@@ -1083,7 +1082,7 @@ public class LogicalGenerator extends SqlBaseBaseListener {
       operator.setKey(ctx.property().ID().getText());
     }
     String value;
-    if(propertyValueContext.STRING_LITERAL() != null) {
+    if (propertyValueContext.STRING_LITERAL() != null) {
       value = removeStringQuote(propertyValueContext.getText());
     } else {
       value = propertyValueContext.getText();
@@ -1146,7 +1145,10 @@ public class LogicalGenerator extends SqlBaseBaseListener {
   @Override
   public void enterKeyValuePair(KeyValuePairContext ctx) {
     CreateTriggerOperator operator = (CreateTriggerOperator) initializedOperator;
-    operator.addParameter(ctx.key.getText(), ctx.value.getText());
+    String key = ctx.key.getText();
+    String value = ctx.value.getText();
+    operator.addParameter(key.substring(1, key.length() - 1),
+        value.substring(1, value.length() - 1));
   }
 
   @Override
